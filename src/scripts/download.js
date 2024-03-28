56c7ce85b4830953b7b37921a57345a485534f7e
Biasiolo <biasiolorafael@gmail.com>
2024-03-28 11:15:08 -0300

    initial

--- src/scripts/download.js
+++ src/scripts/download.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const downloadButton = document.getElementById('downloadButton');
+$(document).ready(function() {
+    const downloadButton = $('#downloadButton');
   
-    downloadButton.addEventListener('click', function() {
+    downloadButton.on('click', function() {
       // Criação do link de download do PDF
-      const link = document.createElement('a');
-      link.href = './assets/Manifesto-de-breton.pdf'; // URL do arquivo PDF
-      link.download = 'Manifesto-de-breton.pdf'; // Nome do arquivo a ser baixado
+      const link = $('<a>', {
+        href: './assets/Manifesto-de-breton.pdf', // URL do arquivo PDF
+        download: 'Manifesto-de-breton.pdf' // Nome do arquivo a ser baixado
+      });
   
       // Adiciona o link à página e simula o clique
-      document.body.appendChild(link);
-      link.click();
+      $('body').append(link);
+      link[0].click();
   
       // Remove o link após o download
       link.remove();
-
+  
       // Exibe o modal de sucesso após o download
       showSuccessModal();
     });
-
+  
     // Função para mostrar o modal de sucesso
     function showSuccessModal() {
-        const successModal = new bootstrap.Modal($("#successModal"));
-      
-        const modalContent = $("#successModalContent");
-        modalContent.text(`Download concluído`);
-      
-        successModal.show();
-      }
-});
+      const modalContent = $('#successModalContent');
+      modalContent.text('Download concluído');
+  
+      $('#successModal').modal('show');
+    }
+  });
+  
